e4b277425b94fd2c1c1c0d1bc6997bb8ffa969dc
slugz <slugz_17@hotmail.com>
2014-11-15 23:09:27 -0500

    Errors cleaned up

--- lovely/src/main/java/com/sakk/lovely/model/forms/ChiropracticOrthodicsExam.java
+++ lovely/src/main/java/com/sakk/lovely/model/forms/ChiropracticOrthodicsExam.java
@@ -4,13 +4,13 @@ import java.io.Serializable;
 
 import com.sakk.lovely.model.BaseEntity;
 
-public class ChiropracticOrthodicsExam extends BaseEntity implements ChiropracticForm, Serializable {
-	
+public class ChiropracticOrthodicsExam extends BaseEntity implements
+		ChiropracticForm, Serializable {
+
 	private static final long serialVersionUID = 2L;
 
 	@Override
 	public void create() {
-		// TODO Auto-generated method stub
 
 	}
 
