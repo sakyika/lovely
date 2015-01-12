afb9e32b2070a8d4cd781f9f3274982f9ffcc69f
Kwadwo Sakyi <kwadwosakyi@gmail.com>
2015-01-11 22:09:43 -0500

    Initial Ajax, Thymeleaf, Tiles configuration

--- lovely/src/main/java/com/sakk/lovely/core/config/Initializer.java
+++ lovely/src/main/java/com/sakk/lovely/core/config/Initializer.java
@@ -1,5 +1,8 @@
 package com.sakk.lovely.core.config;
 
+import javax.servlet.Filter;
+
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class Initializer extends
@@ -7,7 +10,7 @@ public class Initializer extends
 
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
-		return new Class[] { RootConfig.class, SecurityConfig.class };
+		return new Class[] { RootConfig.class, SecurityConfig.class,WebFlowConfig.class };
 	}
 
 	@Override
@@ -18,6 +21,11 @@ public class Initializer extends
 	@Override
 	protected String[] getServletMappings() {
 		return new String[] { "/" };
+	}
+	
+	@Override
+	protected Filter[] getServletFilters() {
+		return new Filter[] { new HiddenHttpMethodFilter() };
 	}
 
 }
--- lovely/src/main/java/com/sakk/lovely/core/config/ThymeleafConfig.java
+++ lovely/src/main/java/com/sakk/lovely/core/config/ThymeleafConfig.java
@@ -1,8 +1,18 @@
 package com.sakk.lovely.core.config;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.dialect.IDialect;
+import org.thymeleaf.extras.conditionalcomments.dialect.ConditionalCommentsDialect;
+import org.thymeleaf.extras.springsecurity3.dialect.SpringSecurityDialect;
+import org.thymeleaf.extras.tiles2.dialect.TilesDialect;
+import org.thymeleaf.extras.tiles2.spring4.web.configurer.ThymeleafTilesConfigurer;
+import org.thymeleaf.extras.tiles2.spring4.web.view.FlowAjaxThymeleafTilesView;
 import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.AjaxThymeleafViewResolver;
 import org.thymeleaf.spring4.view.ThymeleafViewResolver;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 import org.thymeleaf.templateresolver.TemplateResolver;
@@ -22,15 +32,30 @@ public class ThymeleafConfig {
 
 	@Bean
 	public SpringTemplateEngine templateEngine() {
+		
+		Set<IDialect> dialects = new LinkedHashSet<IDialect>();
+		dialects.add(new TilesDialect());
+		dialects.add(new SpringSecurityDialect());
+		dialects.add(new ConditionalCommentsDialect());
+		
 		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
 		templateEngine.setTemplateResolver(templateResolver());
+		templateEngine.setAdditionalDialects(dialects);
 		return templateEngine;
 	}
 
 	@Bean
-	public ThymeleafViewResolver thymeleafViewResolver() {
-		ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+	public ThymeleafViewResolver ajaxThymeleafTilesViewResolver() {
+		ThymeleafViewResolver resolver = new AjaxThymeleafViewResolver();
+		resolver.setViewClass(FlowAjaxThymeleafTilesView.class);
 		resolver.setTemplateEngine(templateEngine());
 		return resolver;
+	}
+	
+	@Bean
+	public ThymeleafTilesConfigurer tilesConfigurer(){
+		ThymeleafTilesConfigurer configurer = new ThymeleafTilesConfigurer();
+		configurer.setDefinitions("/WEB-INF/**/view.xml");
+		return configurer;
 	}
 }
